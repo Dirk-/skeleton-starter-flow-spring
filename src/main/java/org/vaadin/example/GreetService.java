c8b4e38dadb6a039c5df9456f7e521b1b75f5fec
Dirk Fröhling <dirk.froehling@w-hs.de>
2020-06-04 11:55:45 +0000

    Serializable ID

--- src/main/java/org/vaadin/example/GreetService.java
+++ src/main/java/org/vaadin/example/GreetService.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetService implements Serializable {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     public String greet(String name) {
         if (name == null || name.isEmpty()) {
             return "Hallo anonymer User";
